098815d2bbe6d702fe5688ff9a9a99f8a612b028
Raul Souto <raulvprsouto@gmail.com>
2024-01-19 15:58:53 -0300

    commit190124

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { BigCardComponent } from "./components/big-card/big-card.component";
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { ContentComponent } from './pages/content/content.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, BigCardComponent, MenuTitleComponent, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent,ContentComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
