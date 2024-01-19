0f2f31973e0cc1f3cdcd2cc8fa100a25e81d3ade
Raul Souto <raulvprsouto@gmail.com>
2024-01-19 17:37:38 -0300

    ultimaCommmit190124

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { ContentComponent } from './pages/content/content.component';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent,ContentComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, ContentComponent, MenuBarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
--- angular-blog/src/app/pages/home/home.component.ts
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
-import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 import { VerticalCardComponent } from "../../components/vertical-card/vertical-card.component";
 
 @Component({
@@ -11,7 +9,7 @@ import { VerticalCardComponent } from "../../components/vertical-card/vertical-c
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css', './home_responsive.css'],
-    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent, FooterComponent, VerticalCardComponent],
+    imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, VerticalCardComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
