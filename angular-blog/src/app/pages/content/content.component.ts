c8ce0187ed287fd4926d80daf63bee812e93681b
Raul Souto <raulvprsouto@gmail.com>
2024-01-20 20:30:54 -0300

    contentAtualizado

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataArticles} from '../../data/dataArticles';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentMetaDataAuthor:string = ""
+  contentMetaDataDate:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+     this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataArticles.filter(article => article.id == id)[0]
+
+    this.id = result.id;
+    this.photoCover = result.imageUrl;
+    this.contentTitle = result.title;
+    this.contentMetaDataAuthor = result.author;
+    this.contentMetaDataDate = result.publishDate;
+    this.contentDescription = result.content;
+
+  }
 
 }
+
