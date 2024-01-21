f24b28e2f0f9b7344e79f2550df05e743d7b6e13
Raul Souto <raulvprsouto@gmail.com>
2024-01-20 22:43:57 -0300

    ultimacommit200124

--- angular-blog/src/app/data/dataArticles.ts
+++ angular-blog/src/app/data/dataArticles.ts
@@ -10,16 +10,16 @@ export const dataArticles = [
       Projetada como uma máquina de combate biomecânica, essa criação notável é a última defesa da humanidade contra as ameaças dos Anjos.
       Neste artigo, mergulharemos fundo na intrigante jornada da Unidade Evangelion 01, desvendando os mistérios por trás de sua criação
       e testemunhando seu papel crucial como um símbolo de resistência em meio à adversidade.</p>
-      <h2>I. O Surgimento da Unidade Evangelion 01</h2>
+      <h3>I. O Surgimento da Unidade Evangelion 01</h3>
       <p>A criação da Unidade Evangelion 01 marca um marco significativo na história da humanidade. Desenvolvida para enfrentar os Anjos,
       misteriosas entidades que ameaçam a existência humana, esta máquina biomecânica representa a última linha de defesa.</p>
-      <h2>II. O Enigma por Trás da Sua Criação</h2>
+      <h3>II. O Enigma por Trás da Sua Criação</h3>
       <p>A verdade por trás da criação da Unidade Evangelion 01 é um enigma complexo. Quem foram os gênios por trás dessa inovação
       extraordinária? Qual o propósito final dessa máquina de combate única? Explore as teorias e especulações que cercam sua origem.</p>
-      <h2>III.Um Símbolo de Resistência</h2>
+      <h3>III. Um Símbolo de Resistência</h3>
       <p>Além de sua função como uma arma poderosa, a Unidade Evangelion 01 torna-se um símbolo de resistência em um mundo à beira do abismo.
       Descubra como ela inspira esperança em meio ao caos, unindo a humanidade contra uma ameaça iminente.</p>
-      <h2>Conclusão</h2>
+      <h3>Conclusão</h3>
       <p>A jornada da Unidade Evangelion 01 é mais do que uma série de batalhas épicas. É uma narrativa de resistência, inovação e esperança
       em face da adversidade. Enquanto exploramos os mistérios por trás de sua criação, testemunhamos não apenas uma máquina de combate,
       mas um símbolo duradouro da força da humanidade.
@@ -27,4 +27,125 @@ export const dataArticles = [
       tecnologia e significado mais amplo para a sociedade.</p>
     `
   },
+  {
+    "id": "2",
+    "title": "Mapa dos Ataques dos Anjos Revelado",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://steamuserimages-a.akamaihd.net/ugc/1716408815746321638/A2D626CC8D67463064949B8AFAEDF4F4F460EB8A/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+    "content": `
+    <p>No tumultuado cenário de um mundo à beira do colapso, a revelação de um mapa detalhado dos ataques dos Anjos oferece uma visão única e angustiante. Neste artigo, exploraremos os padrões e implicações dessa revelação, desenhando uma narrativa que lança luz sobre a batalha contínua entre a Unidade Evangelion e as misteriosas ameaças angelicais.</p>
+    <h3>I. Padrões dos Ataques</h3>
+    <p>Uma análise profunda revela padrões intrigantes nos ataques dos Anjos, sugerindo estratégias e motivos por trás de suas investidas. Cada ponto no mapa conta uma história de resistência, sacrifício e a incansável busca pela sobrevivência.</p>
+    <h3>II. Consequências Globais</h3>
+    <p>Além da revelação dos padrões, exploraremos as consequências globais desse mapa, examinando como as comunidades ao redor do mundo estão respondendo e se preparando para enfrentar as ameaças iminentes.</p>
+    <h3>Conclusão</h3>
+    <p>Concluímos nossa exploração do mapa dos ataques dos Anjos com uma reflexão sobre o que esse conhecimento significa para a humanidade. Será esse o instrumento que finalmente vira o jogo contra as forças angelicais, ou uma mera peça em um tabuleiro cósmico mais amplo?</p>
+    `
+  },
+  {
+    "id": "3",
+    "title": "Mapa dos Ataques dos Anjos Revelado",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://www.animenewsnetwork.com/thumbnails/crop1200x630gE0/herald/75152/fourth-angel-s-halo-rebuild.png.jpg",
+    "content": `
+    <p>Em um momento que ficará marcado na história, Tokyo é atingida por uma tragédia de proporções devastadoras. Este artigo narra os eventos que levaram a essa tragédia e examina o impacto profundo que ela deixou na sociedade e na psique coletiva.</p>
+<h3>I. O Desencadear dos Eventos</h3>
+<p>Investigaremos os eventos que precederam a tragédia, desde os sinais iniciais até os momentos críticos que desencadearam a catástrofe. Um relato detalhado dos acontecimentos levará os leitores a uma jornada pelos momentos finais de serenidade antes da tempestade.</p>
+<h3>II. Impacto na Sociedade</h3>
+<p>A tragédia não se limitou à destruição física; ela deixou cicatrizes emocionais na sociedade. Examinaremos como as comunidades locais, os serviços de emergência e os cidadãos comuns foram afetados e como estão se recuperando diante dessa adversidade.</p>
+<h3>Conclusão</h3>
+<p>Concluímos nossa análise com uma reflexão sobre a resiliência humana diante da tragédia e como a reconstrução, tanto física quanto emocional, está moldando o futuro de Tokyo e além.</p>
+    `
+  },
+  {
+    "id": "4",
+    "title": "Novas Vagas de Cadetes Abertas!",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://i0.wp.com/codigoespagueti.com/wp-content/uploads/2022/04/Asi-luce-Shinji-Ikari-convertido-en-una-waifu-con-fanart-de-Neon-Genesis-Evangelion-1.jpg",
+    "content": `
+    <p>Em um esforço contínuo para fortalecer as defesas da humanidade, anunciamos a abertura de novas vagas para cadetes. Este artigo detalha as oportunidades emocionantes que aguardam aqueles que ousam se juntar às fileiras dos futuros pilotos de Evangelion.</p>
+    <h3>I. O Chamado à Ação</h3>
+    <p>Exploraremos o chamado à ação, destacando a importância crítica do papel dos cadetes na defesa contra os Anjos. Os aspirantes a pilotos serão inspirados a se voluntariar, enfrentando treinamentos rigorosos para se tornarem os guardiões do futuro.</p>
+    <h3>II. Treinamento Intensivo</h3>
+    <p>Um mergulho profundo no treinamento intensivo revelará os desafios físicos e mentais que os cadetes enfrentarão. Desde simulações de combate até o entendimento avançado da tecnologia Evangelion, cada aspecto do treinamento é projetado para preparar os recrutas para os desafios à frente.</p>
+    `
+  },
+  {
+    "id": "5",
+    "title": "Descobertas Reveladoras: Novos Segredos sobre a Origem dos Anjos em Neon Genesis Evangelion!",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://pm1.aminoapps.com/6568/6a659982bcf5c3af7612573adedb58ee5887f2ef_hq.jpg",
+    "content": `
+    <p>No cenário distópico de um mundo à beira do colapso, surge uma luz de esperança em forma da Unidade Evangelion 01.
+    Projetada como uma máquina de combate biomecânica, essa criação notável é a última defesa da humanidade contra as ameaças dos Anjos.
+    Neste artigo, mergulharemos fundo na intrigante jornada da Unidade Evangelion 01, desvendando os mistérios por trás de sua criação
+    e testemunhando seu papel crucial como um símbolo de resistência em meio à adversidade.</p>
+    <h3>I. O Surgimento da Unidade Evangelion 01</h3>
+    <p>A criação da Unidade Evangelion 01 marca um marco significativo na história da humanidade. Desenvolvida para enfrentar os Anjos,
+    misteriosas entidades que ameaçam a existência humana, esta máquina biomecânica representa a última linha de defesa.</p>
+    <h3>II. O Enigma por Trás da Sua Criação</h3>
+    <p>A verdade por trás da criação da Unidade Evangelion 01 é um enigma complexo. Quem foram os gênios por trás dessa inovação
+    extraordinária? Qual o propósito final dessa máquina de combate única? Explore as teorias e especulações que cercam sua origem.</p>
+    <h3>III. Um Símbolo de Resistência</h3>
+    <p>Além de sua função como uma arma poderosa, a Unidade Evangelion 01 torna-se um símbolo de resistência em um mundo à beira do abismo.
+    Descubra como ela inspira esperança em meio ao caos, unindo a humanidade contra uma ameaça iminente.</p>
+    <h3>Conclusão</h3>
+    <p>A jornada da Unidade Evangelion 01 é mais do que uma série de batalhas épicas. É uma narrativa de resistência, inovação e esperança
+    em face da adversidade. Enquanto exploramos os mistérios por trás de sua criação, testemunhamos não apenas uma máquina de combate,
+    mas um símbolo duradouro da força da humanidade.
+    <br>Este artigo fictício explora o universo da Unidade Evangelion 01, proporcionando uma visão aprofundada de sua história,
+    tecnologia e significado mais amplo para a sociedade.</p>
+    `
+  },
+  {
+    "id": "6",
+    "title": "Pilotos em Destaque: As Habilidades Extraordinárias dos Jovens Defensores das Unidades Evangelion",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://criticalhits.com.br/wp-content/uploads/2023/11/maxresdefault-56.jpg",
+    "content": `
+    <p>Em um momento que ficará marcado na história, Tokyo é atingida por uma tragédia de proporções devastadoras. Este artigo narra os eventos que levaram a essa tragédia e examina o impacto profundo que ela deixou na sociedade e na psique coletiva.</p>
+<h3>I. O Desencadear dos Eventos</h3>
+<p>Investigaremos os eventos que precederam a tragédia, desde os sinais iniciais até os momentos críticos que desencadearam a catástrofe. Um relato detalhado dos acontecimentos levará os leitores a uma jornada pelos momentos finais de serenidade antes da tempestade.</p>
+<h3>II. Impacto na Sociedade</h3>
+<p>A tragédia não se limitou à destruição física; ela deixou cicatrizes emocionais na sociedade. Examinaremos como as comunidades locais, os serviços de emergência e os cidadãos comuns foram afetados e como estão se recuperando diante dessa adversidade.</p>
+<h3>Conclusão</h3>
+<p>Concluímos nossa análise com uma reflexão sobre a resiliência humana diante da tragédia e como a reconstrução, tanto física quanto emocional, está moldando o futuro de Tokyo e além.</p>
+    `
+  },
+  {
+    "id": "7",
+    "title": "Mistérios Desvendados: As Implicações Filosóficas por Trás do Enigma da Instrumentalidade",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://img.olhardigital.com.br/wp-content/uploads/2023/06/the-end-of-evangelion-1024x576.jpg",
+    "content": `
+    <p>No tumultuado cenário de um mundo à beira do colapso, a revelação de um mapa detalhado dos ataques dos Anjos oferece uma visão única e angustiante. Neste artigo, exploraremos os padrões e implicações dessa revelação, desenhando uma narrativa que lança luz sobre a batalha contínua entre a Unidade Evangelion e as misteriosas ameaças angelicais.</p>
+    <h3>I. Padrões dos Ataques</h3>
+    <p>Uma análise profunda revela padrões intrigantes nos ataques dos Anjos, sugerindo estratégias e motivos por trás de suas investidas. Cada ponto no mapa conta uma história de resistência, sacrifício e a incansável busca pela sobrevivência.</p>
+    <h3>II. Consequências Globais</h3>
+    <p>Além da revelação dos padrões, exploraremos as consequências globais desse mapa, examinando como as comunidades ao redor do mundo estão respondendo e se preparando para enfrentar as ameaças iminentes.</p>
+    <h3>Conclusão</h3>
+    <p>Concluímos nossa exploração do mapa dos ataques dos Anjos com uma reflexão sobre o que esse conhecimento significa para a humanidade. Será esse o instrumento que finalmente vira o jogo contra as forças angelicais, ou uma mera peça em um tabuleiro cósmico mais amplo?</p>
+    `
+  },
+  {
+    "id": "8",
+    "title": "Desafios Épicos à Vista: Previsões para o Próximo Confronto dos Evangelions com os Anjos",
+    "author": "Raul Souto",
+    "publishDate": "20 de jan. de 2024",
+    "imageUrl": "https://super.abril.com.br/wp-content/uploads/2018/07/56b3ab3e0e21630a3e115e3feva1.jpeg",
+    "content": `
+    <p>Em um esforço contínuo para fortalecer as defesas da humanidade, anunciamos a abertura de novas vagas para cadetes. Este artigo detalha as oportunidades emocionantes que aguardam aqueles que ousam se juntar às fileiras dos futuros pilotos de Evangelion.</p>
+<h3>I. O Chamado à Ação</h3>
+<p>Exploraremos o chamado à ação, destacando a importância crítica do papel dos cadetes na defesa contra os Anjos. Os aspirantes a pilotos serão inspirados a se voluntariar, enfrentando treinamentos rigorosos para se tornarem os guardiões do futuro.</p>
+<h3>II. Treinamento Intensivo</h3>
+<p>Um mergulho profundo no treinamento intensivo revelará os desafios físicos e mentais que os cadetes enfrentarão. Desde simulações de combate até o entendimento avançado da tecnologia Evangelion, cada aspecto do treinamento é projetado para preparar os recrutas para os desafios à frente.</p>
+    `
+  },
+
 ];
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {dataArticles} from '../../data/dataArticles';
 import { RouterModule } from '@angular/router';
+import { VerticalCardComponent } from "../../components/vertical-card/vertical-card.component";
 @Component({
-  selector: 'app-content',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+    selector: 'app-content',
+    standalone: true,
+    templateUrl: './content.component.html',
+    styleUrl: './content.component.css',
+    imports: [RouterModule, VerticalCardComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ContentComponent implements OnInit {
   photoCover: string = "";
@@ -31,10 +33,10 @@ export class ContentComponent implements OnInit {
     if (result) {
       this.id = result.id;
       this.photoCover = result.imageUrl;
-      this.contentTitle = result.title;
+      this.contentTitle = result.title.toUpperCase();
       this.contentMetaDataAuthor = result.author;
       this.contentMetaDataDate = result.publishDate;
       this.contentDescription = result.content;
-    }
   }
+}
 }
